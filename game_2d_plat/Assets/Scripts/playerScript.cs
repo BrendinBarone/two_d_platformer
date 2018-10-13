a610b0b5aaa884590ede0ceb7e3f0ed1282f6b7e
RobotCity <robotcityhq@gmail.com>
2018-10-12 22:39:37 -0500

    movement script pt 1

---
+++ game_2d_plat/Assets/Scripts/playerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// movemenet variables
+	public float maxSpeed;
+
+	Rigidbody2D myRB;
+	Animator myAnim;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = input.getAxis ("Horizontal");
+
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+	}
+}
